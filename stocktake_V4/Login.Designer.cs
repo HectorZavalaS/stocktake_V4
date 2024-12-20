b08df2b8dbd35d12c1de60614e427c04967ae2a2
Héctor Zavala <hector.zavala@siix-global.com>
2024-12-20 11:11:52 -0600

    Agregar archivos de proyecto.

---
+++ stocktake_V4/Login.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace stocktake_V4
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.btnAccept = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.btnExit = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonGroupBox1 = new ComponentFactory.Krypton.Toolkit.KryptonGroupBox();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.txtPass = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.txtUser = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.kryptonPanel4 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.selMag = new ComponentFactory.Krypton.Toolkit.KryptonRadioButton();
+            this.selBox = new ComponentFactory.Krypton.Toolkit.KryptonRadioButton();
+            this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1.Panel)).BeginInit();
+            this.kryptonGroupBox1.Panel.SuspendLayout();
+            this.kryptonGroupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel4)).BeginInit();
+            this.kryptonPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(280, 170);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(90, 25);
+            this.btnAccept.TabIndex = 0;
+            this.btnAccept.Values.Text = "Aceptar";
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            this.btnAccept.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnAccept_KeyPress);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(160, 170);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 25);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Values.Text = "Salir";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // kryptonGroupBox1
+            // 
+            this.kryptonGroupBox1.Location = new System.Drawing.Point(19, 46);
+            this.kryptonGroupBox1.Name = "kryptonGroupBox1";
+            // 
+            // kryptonGroupBox1.Panel
+            // 
+            this.kryptonGroupBox1.Panel.Controls.Add(this.kryptonLabel2);
+            this.kryptonGroupBox1.Panel.Controls.Add(this.kryptonLabel1);
+            this.kryptonGroupBox1.Panel.Controls.Add(this.txtPass);
+            this.kryptonGroupBox1.Panel.Controls.Add(this.txtUser);
+            this.kryptonGroupBox1.Size = new System.Drawing.Size(231, 106);
+            this.kryptonGroupBox1.TabIndex = 2;
+            this.kryptonGroupBox1.Values.Heading = "";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(5, 59);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(65, 20);
+            this.kryptonLabel2.TabIndex = 3;
+            this.kryptonLabel2.Values.Text = "Password:";
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(32, 23);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(38, 20);
+            this.kryptonLabel1.TabIndex = 2;
+            this.kryptonLabel1.Values.Text = "User:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtPass.Location = new System.Drawing.Point(76, 59);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(133, 23);
+            this.txtPass.TabIndex = 1;
+            this.txtPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPass_KeyPress);
+            // 
+            // txtUser
+            // 
+            this.txtUser.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtUser.Location = new System.Drawing.Point(76, 20);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(133, 23);
+            this.txtUser.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(259, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 173);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(122, 22);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // kryptonPanel4
+            // 
+            this.kryptonPanel4.Controls.Add(this.selMag);
+            this.kryptonPanel4.Controls.Add(this.selBox);
+            this.kryptonPanel4.Location = new System.Drawing.Point(278, 69);
+            this.kryptonPanel4.Name = "kryptonPanel4";
+            this.kryptonPanel4.Size = new System.Drawing.Size(93, 83);
+            this.kryptonPanel4.TabIndex = 14;
+            // 
+            // selMag
+            // 
+            this.selMag.Checked = true;
+            this.selMag.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.BoldControl;
+            this.selMag.Location = new System.Drawing.Point(13, 46);
+            this.selMag.Name = "selMag";
+            this.selMag.Size = new System.Drawing.Size(79, 20);
+            this.selMag.TabIndex = 1;
+            this.selMag.Values.Text = "Magazine";
+            this.selMag.CheckedChanged += new System.EventHandler(this.selMag_CheckedChanged);
+            // 
+            // selBox
+            // 
+            this.selBox.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.BoldControl;
+            this.selBox.Location = new System.Drawing.Point(13, 19);
+            this.selBox.Name = "selBox";
+            this.selBox.Size = new System.Drawing.Size(48, 20);
+            this.selBox.TabIndex = 0;
+            this.selBox.Values.Text = "BOX";
+            this.selBox.CheckedChanged += new System.EventHandler(this.selBox_CheckedChanged);
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.LabelStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.BoldControl;
+            this.kryptonLabel3.Location = new System.Drawing.Point(275, 47);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(107, 20);
+            this.kryptonLabel3.TabIndex = 15;
+            this.kryptonLabel3.Values.Text = "Tipo de escaneo:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::stocktake_V4.Properties.Resources.backHeader;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(395, 207);
+            this.Controls.Add(this.kryptonLabel3);
+            this.Controls.Add(this.kryptonPanel4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.kryptonGroupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAccept);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.Shown += new System.EventHandler(this.Login_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1.Panel)).EndInit();
+            this.kryptonGroupBox1.Panel.ResumeLayout(false);
+            this.kryptonGroupBox1.Panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1)).EndInit();
+            this.kryptonGroupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel4)).EndInit();
+            this.kryptonPanel4.ResumeLayout(false);
+            this.kryptonPanel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnAccept;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton btnExit;
+        private ComponentFactory.Krypton.Toolkit.KryptonGroupBox kryptonGroupBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtPass;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox txtUser;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kryptonPanel4;
+        private ComponentFactory.Krypton.Toolkit.KryptonRadioButton selMag;
+        private ComponentFactory.Krypton.Toolkit.KryptonRadioButton selBox;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel3;
+    }
+}
---
+++ stocktake_V4/Login.cs
@@ -0,0 +1,163 @@
+﻿using stocktake_V4.Class;
+using stocktake_V4.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stocktake_V4
+{
+    public partial class Login : Form
+    {
+        Thread splashthread;
+        int m_type_scan;
+        CAlert m_alerts;
+        public Login()
+        {
+            InitializeComponent();
+            m_type_scan = 2;
+            m_alerts = new CAlert();
+        }
+        public static string GetSHA1(String texto)
+        {
+            SHA1 sha1 = SHA1CryptoServiceProvider.Create();
+            Byte[] textOriginal = ASCIIEncoding.Default.GetBytes(texto);
+            Byte[] hash = sha1.ComputeHash(textOriginal);
+            StringBuilder cadena = new StringBuilder();
+
+            foreach (byte i in hash)
+            {
+                cadena.AppendFormat("{0:x2}", i);
+            }
+            return cadena.ToString();
+        }
+        private void Login_Load(object sender, EventArgs e)
+        {
+            txtUser.Focus();
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void login()
+        {
+            int idUser = 0;
+            int level = 0;
+            String userName = "";
+            if (String.IsNullOrEmpty(txtUser.Text))
+            {
+                m_alerts.playWarning();
+                MessageBox.Show("Debes ingresar un usuario.", "Login...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUser.Focus();
+                return;
+            }
+
+            if (String.IsNullOrEmpty(txtPass.Text))
+            {
+                m_alerts.playWarning();
+                MessageBox.Show("Debes ingresar tu password.", "Login...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtPass.Focus();
+                return;
+            }
+            progressBar1.Visible = true;
+            //splashthread = new Thread(new ThreadStart(SplashScreen.ShowSplashScreen));
+            //splashthread.IsBackground = true;
+            //splashthread.Start();
+            if (SignInDB(txtUser.Text.ToUpper(), txtPass.Text.ToUpper(), ref idUser, ref userName, ref level))
+            {
+
+                Form1 Main = new Form1(idUser, userName, level,m_type_scan);
+                this.Hide();
+                try
+                {
+                    //splashthread.Abort();
+                    //SplashScreen.CloseSplashScreen();
+                    m_alerts.playSuccess();
+                    Main.ShowDialog();
+                    Cursor = Cursors.Arrow;
+                }
+                catch(Exception ex)
+                {
+                    m_alerts.playWarning();
+                    MessageBox.Show(ex.Message);
+                }
+                this.Show();
+                txtUser.Text = "";
+                txtPass.Text = "";
+                txtUser.Focus();
+            }
+            else
+            {
+                m_alerts.playWarning();
+                MessageBox.Show("Usuario o password incorrectos.", "Login...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUser.Text = "";
+                txtPass.Text = "";
+                txtUser.Focus();
+            }
+            //splashthread.Abort();
+            //SplashScreen.CloseSplashScreen();
+            progressBar1.Visible = false;
+        }
+
+        private bool SignInDB(String user, string pass, ref int idUser, ref String userName, ref int level)
+        {
+            siixsem_main_dbEntities db = new siixsem_main_dbEntities();
+            string strPass = GetSHA1(pass);
+            Cursor = Cursors.WaitCursor;
+            validate_user_Result res = db.validate_user(user, strPass).First();
+            //Cursor = Cursors.Arrow;
+
+            idUser = res.se_id_user;
+            userName = res.se_name;
+            level = res.se_level;
+            
+
+            return res.RESULT == 1 ? true : false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                login();
+            }
+        }
+
+        private void btnAccept_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                login();
+            }
+        }
+
+        private void Login_Shown(object sender, EventArgs e)
+        {
+            txtUser.Focus();
+        }
+
+        private void selBox_CheckedChanged(object sender, EventArgs e)
+        {
+            m_type_scan = 1;
+        }
+
+        private void selMag_CheckedChanged(object sender, EventArgs e)
+        {
+            m_type_scan = 2;
+        }
+    }
+}
